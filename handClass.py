1b47e3f5437c02c48474c75d423de4748b55e511
TheGoulashGuy <holden.galusha@gmail.com>
2017-11-21 23:32:37 -0500

    further improvements

--- handClass.py
+++ handClass.py
@@ -7,17 +7,19 @@ class Hand(object):
 		self.name = name
 		self.bet = bet
 		self.hand = list()
-		self.value = 0	
+		self.value = 0
 
 	def dealCard(self):
 		self.new_card = deckClass.deck.pop()
 		self.hand.append(self.new_card)
+		self.value += self.new_card
 
-	def calcHandValue(self):
-		for key in self.hand:
-			self.newValue = int(deckClass.ranks.get(key[0]))
-			self.value += self.newValue
+	''''def calcHandValue(self):
+		for integer in self.hand:
+		self.newValue = self.hand
+		print(self.newValue)
+		self.value += self.newValue'''
 
 	def showHand(self):
-		print(self.hand)
-		print(self.value)
+		print("Your hand consists of: " + str(self.hand))
+		print("It's value is: " + str(self.value))
--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
 
-import handClass #Don't forget to import winCheck and other needed modules
+import handClass, deckClass #Don't forget to import winCheck and other needed modules
 
 print('''
-Welcome to Blacksnake, a Python implementation of Blackjack. *Gambing is not encouraged*
+Welcome to Blacksnake, a Python implementation of Blackjack. *Gambling is not encouraged*
 ''')
+
+def winCheck():
+	pass
+
 player_name_input = input("Enter your name: ")
 player_bet_input = input("Enter the amount of money you'd like to bet: ")
 player_hand = handClass.Hand(str(player_name_input), int(player_bet_input))
 dealer_hand = handClass.Hand('dealer', 0)
+
+def playerHits():
+	player_hand.dealCard()
+	player_hand.calcHandValue()
+	player_hand.showHand()
 
 deckClass.new_deck.createDeck() #This creates the card deck and shuffles it.
 player_hand.dealCard() #This deals 1st of player's 2 initial cards
 player_hand.dealCard() #This deals 2nd of player's 2 initial cards
 dealer_hand.dealCard()
 dealer_hand.dealCard()
+player_hand.calcHandValue()
+player_hand.showHand()
 
 game_in_session = True
 while game_in_session == True:
 	playerChoice = input("Would you like to stand (s) or hit (h)? ")
 	if playerChoice == 'h':
-		player_hand.dealCard()
-		player_hand.calcHandValue()
-		player_hand.showHand()
+		playerHits()
 	elif playerChoice == 's':
-		#winCheck.wincheck() #This function will set game_in_session to false if someone has won, thus ending the main game loop.
+		#wincheck() #This function will set game_in_session to false if someone has won, thus ending the main game loop.
 		pass
 	else:
 		print('Error, please input "h" or "s": ')
 		print(playerChoice)
-		
-	
-
